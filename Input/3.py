472d0d08900beaa8af8ea42979146567c9bf8dd9
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-03 06:11:55 +0530

    Input

---
+++ Input/3.py
@@ -0,0 +1,10 @@
+# 3. Write a python script which takes two numbers from the user, then calculate their sum and display the result.
+
+
+x=int(input("Enter a Number\t"))
+y=int(input("Enter a Number\t"))
+'''
+z=x+y
+print(y)
+'''
+print(x+y)
---
+++ Input/4.py
@@ -0,0 +1,6 @@
+# 4. Write a python script which takes the radius from the user and display area of a circle.
+
+pie=3.14
+r=int(input("Enter value of Radius\t"))
+area=pie*r**2
+print("The Area of Circle is =",area)
