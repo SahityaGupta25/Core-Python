57098035274f06eb625a26c7228e1a17d7cbd380
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-03 06:15:19 +0530

    Input

---
+++ 10.py
@@ -0,0 +1,8 @@
+# 10. Write a python script to calculate area of a rectangle
+
+# L * B
+
+l=int(input("Enter Value of Length\t"))
+w=int(input("Enter Value of Width\t"))
+a=l*w
+print("The Area of Rectangle is =",a)
