ce698d22e9e77812ebbcf83744904c5d63f8f022
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-05 20:30:19 +0530

    Operators

---
+++ Operators/10.py
@@ -0,0 +1,5 @@
+# 10. Write a python script to use IS operator to display if both variables are the same object or not?
+
+x=66
+y=66
+print(x is y)
---
+++ Operators/9.py
@@ -0,0 +1,5 @@
+# 9. Write a python script to use NOT IN operator to display the data not present in list
+
+x=[3,4,5,6,54,66,16,25]
+y= 26 not in x
+print(y)
