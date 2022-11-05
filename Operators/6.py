e9cb6de33643e159c577d4d23e7c01c7fbd80de6
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-05 20:29:49 +0530

    Operators

---
+++ Operators/6.py
@@ -0,0 +1,6 @@
+# 6. Write a python script which takes a three digit number from the user and displays only its middle digit.
+
+x=123
+y=x//10
+z=y%10
+print("The Middle Digit is =",z)
---
+++ Operators/8.py
@@ -0,0 +1,5 @@
+# 8. Write a python script to use IN operator to display the data present in the list
+
+x=[3,4,5,6,54,66,16,25]
+y= 26 in x
+print(y)
