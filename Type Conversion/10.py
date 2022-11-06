65a6a69171948d79b2dc3cb6674290a25e5acb97
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-06 14:22:45 +0530

    Type Conversion

---
+++ Type Conversion/10.py
@@ -0,0 +1,7 @@
+# 10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and display the result in binary format.
+
+# 0o31 0x27
+x=int(0o31)
+y=int(0x27)
+z=bin(x+y)
+print(z)
