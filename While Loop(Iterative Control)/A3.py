da54025b87c8f66a92c980ab6868f7a676f9969a
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-12 17:52:44 +0530

    A3-A4

---
+++ While Loop(Iterative Control)/A3.py
@@ -0,0 +1,14 @@
+# Print 10 Natural Numbers in Reverse Order
+
+# 1st Approach
+'''
+i=10
+while i>=1:
+    print(i,end=' ')
+    i-=1
+'''
+# 2nd Approach
+i=1
+while i<=10:
+    print(11-i,end=' ')
+    i+=1
---
+++ While Loop(Iterative Control)/A4.py
@@ -0,0 +1,10 @@
+# Write a Program to Print n Number of Even Natural Numbers
+
+# 1st Approach
+
+i=2
+n=int(input("Enter a Number\t"))
+while (i<=n*2):
+    print(i,end=' ')
+    i+=2
+    
