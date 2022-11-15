2f4794bea76149bcd0fc6623be54f5068360e430
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-15 20:10:54 +0530

    Range

---
+++ For Loop & Range/A4.py
@@ -0,0 +1,5 @@
+# Write a Program to Print N Natural Numbers
+
+x=int(input("Enter a Number\t"))
+for y in range(1,x+1,1) :
+    print(y,end=' ')
---
+++ For Loop & Range/A6.py
@@ -0,0 +1,6 @@
+# Write a Program to Print Sqaures N Natural Numbers.
+
+
+x=int(input("Enter a Number\t"))
+for y in range(1,x+1) :
+    print(y**2,end=' ')
