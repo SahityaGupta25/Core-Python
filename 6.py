825ccb2b8c835efc010ac0ea1c503b6e65a5cc13
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-21 12:16:00 +0530

    6.py

--- 6.py
+++
@@ -1,9 +0,0 @@
-# 6. Write a python program to append elements from another list to the current list.( firstlist = ["Java", "Python", "SQL"] secondlist = ["C", "Cpp", "NoSQL"] )
-
-firstlist = ["Java", "Python", "SQL"]
-secondlist = ["C", "Cpp", "NoSQL"] 
-i=0
-while (i<3):
-    firstlist.append(secondlist[i])
-    i+=1
-print(firstlist)
