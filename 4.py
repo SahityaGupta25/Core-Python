f49008493a20725330b46cb412ab1a688b17d92c
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-21 12:15:30 +0530

    4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-# 4. Write a Python script to find the greatest number in a given list of numbers.
-
-a=int(input("Enter Length of List ofNumbers\t"))
-i=1
-l1=[]
-while (i<=a):
-    l1.append(int(input("Enter Value")))
-    i+=1
-print("The Greatest No. in the list is =",max(l1))
