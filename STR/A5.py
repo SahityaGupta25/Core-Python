60a6dd23d74a3ac69ee287a13ad42547477e9e19
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-23 06:29:30 +0530

    STR

---
+++ STR/A5.py
@@ -0,0 +1,12 @@
+# Split & Join
+
+# x='hjakkijakjkijiajjaijijiajijjaijijaijihuayftfadrdresdas'
+# y=x.split('a')
+# print(y)
+# --------------------------------------------------------
+
+# Join
+
+z=['hj', 'kkij', 'kjkiji', 'jj', 'ijiji', 'jijj', 'ijij', 'ijihu', 'yftf', 'drdresd', 's']
+a='k'.join(z)
+print(a)
---
+++ STR/A6.py
@@ -0,0 +1,3 @@
+
+x="8076"
+print( [int(e) for e in x if ord(e)>=48 and ord(e)<=57 ])  
