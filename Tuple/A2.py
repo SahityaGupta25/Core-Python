d8972a4c93d8d26712f8b51c6abcdeb1d14836db
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-24 17:49:04 +0530

    A2,A3

---
+++ Tuple/A2.py
@@ -0,0 +1,1 @@
+print(tuple([int(x) for x in input("Enter Values Seperated by ','\n").split(',')]))
---
+++ Tuple/A3.py
@@ -0,0 +1,7 @@
+# x=[3,5,7,67,34,56,787798,908,54,3]
+# print(tuple(x))
+
+print(tuple(range(1,11)))
+
+y=tuple(input("Enter Value\t"))
+print(y)
