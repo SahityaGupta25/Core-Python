3fb80c28e1a95f6c2649366c6c52141eb3b59009
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-24 17:27:10 +0530

    5,A1

---
+++ Tuple/5.py
@@ -0,0 +1,8 @@
+# 5. Write a python program to check if all items in the tuple are the same.
+
+a=(1,2,3,4)
+b=(1,2,3,4)
+if a==b:
+    print("Same Tuple")
+else:
+    print("Not Same")
---
+++ Tuple/A1.py
@@ -0,0 +1,11 @@
+# Accessing Tuple Elements
+
+x=(66,25,16,9,7,2)
+# i=0
+# while (i<len(x)) :
+#     print(x[i],end=' ')
+#     i+=1
+
+
+for y in x:
+    print(y,end=' ')
