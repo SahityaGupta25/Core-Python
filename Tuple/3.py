ebe33f26ec426b7088d2e66489615cd3ebe151c7
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-24 17:26:37 +0530

    3,4

---
+++ Tuple/3.py
@@ -0,0 +1,5 @@
+# 3. Write a python program to reverse the tuple.
+
+x=(63,7,34,43,56,65,676,76,93)
+y=x[-1: :-1]
+print(y)
---
+++ Tuple/4.py
@@ -0,0 +1,6 @@
+# 4. Write a python program to Swap two tuples in Python.
+
+a=(1,2,3,4)
+b=(5,6,7,8)
+a,b=b,a
+print(a,b)
