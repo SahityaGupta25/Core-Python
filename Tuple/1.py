d1073e564a7e68cd3ed2fd744073f0d73a9dbae4
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-24 17:26:11 +0530

    1,2

---
+++ Tuple/1.py
@@ -0,0 +1,8 @@
+# 1. Write a python script to store multiple items in a single variable ( Items are “Java”,“Python”, “SQL”, “C” ) using tuple
+
+t=(tuple([str(x) for x in input("Enter Values Seperated by ','\n").split(',')]))
+a=()
+a=a+t
+print(a)
+
+
