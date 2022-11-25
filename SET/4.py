62c36e0414ce659b495db5b14ca740d007f5b2bd
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-25 10:16:52 +0530

    SET

---
+++ SET/4.py
@@ -0,0 +1,5 @@
+# 4. Write a Python script to find if “Python” is present in the set thisset = {"Java","Python", "Django"}
+
+thisset = {"Java","Python", "Django"}
+x={"Yes Their is Python in this Set" for y in thisset if "Python" in y }
+print(x)
---
+++ SET/5.py
@@ -0,0 +1,6 @@
+# 5. Write a python program to add items from another set to the current set. thisset = {"Java", "Python", "SQL"}
+
+thisset = {"Java", "Python", "SQL"}
+s1={67, 36,(3+4j),"RAM",6.6,True}
+x=thisset.update(s1)
+print(thisset)
