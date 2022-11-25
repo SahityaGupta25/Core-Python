1868ec0f73d8de9019cbe150d2ec6322af374528
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-25 10:15:04 +0530

    SET

---
+++ SET/1.py
@@ -0,0 +1,5 @@
+# 1. Write a python program to store all the programming languages known to you using Set.
+
+z=input("Enter Names of Programming languages & Enter using ','\n")
+x={eval(y) for y in z.split(',') }
+print(x)
