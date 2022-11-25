0c5980ad118e36ffc4235a79a6820e4d08fa4458
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-25 10:15:51 +0530

    SET

---
+++ SET/2.py
@@ -0,0 +1,5 @@
+# 2. Write a python program to store your own information {name, age, gender, so on..}
+
+z=input("Enter your Name , Age , Gender \n")
+x={eval(y) for y in z.split(',') }
+print(x)
---
+++ SET/3.py
@@ -0,0 +1,6 @@
+# 3. Write a python script to get the data type of a Set.
+
+
+s1={67, 36,(3+4j),"RAM",6.6,True}
+x={type(y) for y in s1 }
+print(x)
