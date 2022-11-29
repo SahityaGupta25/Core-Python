edc00b37924d4e3ea5665cd5a2cf460d1dddce81
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-29 11:53:00 +0530

    A3,A4

---
+++ Dict/A3.py
@@ -0,0 +1,21 @@
+d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+
+d2=dict(a=55,b=33,c=78,f=66)
+
+
+# Min
+
+print(min(d2))
+
+# Max
+print(max(d2))
+
+# Sorted
+
+print(sorted (d2))
+
+# SUM
+#print(sum(d2))
+
+# LEN
+print(len (d2))
---
+++ Dict/A4.py
@@ -0,0 +1,9 @@
+# d2=dict(a=55,b=33,c=78,f=66)
+# d3=dict(a=55,b=33,c=78,f=66)
+# print(d2,d3)
+# print(d2==d3)
+
+d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+d2={True: 'vasu', False: 6, 'Sahitya g': 23, 9.3: 'hi'}
+print(d1,d2,end='\n')
+print(d1==d2)
