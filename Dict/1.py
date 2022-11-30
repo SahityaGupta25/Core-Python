05ef5221769599a708ef586ea828fdf5386986fb
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-30 10:45:20 +0530

    1,2

---
+++ Dict/1.py
@@ -0,0 +1,13 @@
+# 1. Write a python program to create and print a dictionary which stores your information.
+# (name:gender, age:weight)
+
+dt={}
+
+y=input("Enter Your Name\t")
+z=input("Enter Your Gender\t")
+k=int(input("Enter Your Age\t"))
+d=int(input("Enter Your Weight\t"))
+dt[y]=z
+dt[k]=d
+print()
+print(dt)
---
+++ Dict/2.py
@@ -0,0 +1,6 @@
+# 2. Write a python program to access the items of a dictionary by referring to its key name. d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+
+d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+
+for a in d1:
+    print(a,"is a 'Key Value' and its 'Data value' is=",d1[a])
