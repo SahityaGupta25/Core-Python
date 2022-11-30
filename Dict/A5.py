28cf0029c75bf86ec99c4c31d2c6575c6667cf63
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-30 10:44:14 +0530

    A5,A6

---
+++ Dict/A5.py
@@ -0,0 +1,19 @@
+# POP
+
+# d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+# print(d1.pop(9.3))
+# print(d1)
+
+
+# POP ITEM
+
+
+# d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+# x=d1.popitem()
+# print(d1)
+
+# CLEAR
+
+d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+x=d1.clear()
+print(d1)
---
+++ Dict/A6.py
@@ -0,0 +1,8 @@
+n=int(input("Enter No. of Students\t"))
+dt={}
+for x in range(n):
+    k=int(input("Enter Roll No.\t"))
+    d=input("Enter Name of Student\t")
+    dt[k]=d
+    print()
+print(dt)
