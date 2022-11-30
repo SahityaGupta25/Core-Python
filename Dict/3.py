2204d7077b6b063aab2ab0df2041b1b099fdf2b5
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-11-30 10:45:44 +0530

    3,4

---
+++ Dict/3.py
@@ -0,0 +1,5 @@
+# 3. Write a python program to get a list of the values from a dictionary.d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+
+d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+x=d1.values 
+print(x)
---
+++ Dict/4.py
@@ -0,0 +1,5 @@
+# 4. Write a python program to change the value of a specific item by referring to its key name.d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+
+d1={True: 'vasu', False: 6, 'Sahitya': 23, 9.3: 'hi'}
+d1[False]='Ram'
+print(d1)
