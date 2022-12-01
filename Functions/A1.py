e165252c06add4b6b85f239707e0a3f4cf38da30
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-01 14:37:39 +0530

    A1

---
+++ Functions/A1.py
@@ -0,0 +1,11 @@
+# Syntax of Function
+
+#1. Takes Nothing Returns Nothing
+
+def add():
+    a=int(input("Enter a Number\t"))
+    b=int(input("Enter a Number\t"))
+    c=a+b
+    print(c)
+
+add()
