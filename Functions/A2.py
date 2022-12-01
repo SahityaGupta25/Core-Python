fc7e60cb7c2d347c6b9546c6a5522fe8854be141
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-01 14:38:20 +0530

    A2.A3

---
+++ Functions/A2.py
@@ -0,0 +1,9 @@
+#1. Takes Nothing Returns Nothing
+
+# Write a python program to create a simple function which prints “Krishna” .
+
+def god():
+    print("Krishna")
+
+god()
+
---
+++ Functions/A3.py
@@ -0,0 +1,7 @@
+# 2. Takes Something Returns Nothing
+
+def sub(a,b):
+    c=a-b
+    print(c)
+
+sub(4,7)
