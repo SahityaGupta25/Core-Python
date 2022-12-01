4cead4d41888b3989e2211ae8e159aa3c13efa67
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-01 14:38:55 +0530

    a4,A5

---
+++ Functions/A4.py
@@ -0,0 +1,7 @@
+# Write a python program to create a function which expects two arguments and print them in the function body.
+
+def mul(a,b):
+    c=a*b
+    print(c)
+
+mul(2,5)
---
+++ Functions/A5.py
@@ -0,0 +1,9 @@
+# 3. Takes Nothing Returns Something
+
+def mul():
+    a=int(input("Enter a Number\t"))
+    b=int(input("Enter a Number\t"))
+    c=a*b
+    return c
+
+print("This is the Result",mul())
