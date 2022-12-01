2eca8a1d1eab88d85db2e7626992ed56a3f39665
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-01 14:40:04 +0530

    A8

---
+++ Functions/A8.py
@@ -0,0 +1,15 @@
+# 1. Takes Nothing Returns Nothing
+
+
+# Write a python program to create a function that finds a maximum of four numbers.
+
+def m():
+    a=int(input("Enter a Number\t"))
+    b=int(input("Enter a Number\t"))
+    c=int(input("Enter a Number\t"))
+    d=int(input("Enter a Number\t"))
+    e=max(a,b,c,d)
+    print(e)
+
+m()    
+
