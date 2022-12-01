00d52de8db4b1be8228adde87ae68cfde1f627a5
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-01 14:39:43 +0530

    A6,A7

---
+++ Functions/A6.py
@@ -0,0 +1,12 @@
+# 1. Takes Nothing Returns Nothing
+
+# Write a python program to create a function to check whether a given number is even or odd.
+
+def evdd():
+    a=int(input("Enter a Number\t"))
+    if (a%2==0) :
+        print("Even")
+    else :
+        print("Odd")
+
+evdd()
---
+++ Functions/A7.py
@@ -0,0 +1,7 @@
+# 4. Takes Something Returns Something
+
+def add(a,b):
+    c=a+b
+    return c
+
+print(add(66,34))
