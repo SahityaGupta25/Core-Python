f6df609ef1df173dbec138d80fca52451dc2316b
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-02 09:54:17 +0530

    A12,A13

---
+++ Functions/A12.py
@@ -0,0 +1,9 @@
+# Keyword Argument
+# ----------------
+
+
+def add(a,b,d=66,e=51):
+    c=a+b+d+e
+    print(c)
+
+add(5,4,d=14)
---
+++ Functions/A13.py
@@ -0,0 +1,7 @@
+# Write a python program to create a function which expects kwargs arguments.
+
+def sq(x,y):
+    z=x**y
+    print(z)
+
+sq(y=4,x=66)
