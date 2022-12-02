bb83a83f34551b73efa26604269b809c7047c335
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-02 09:52:12 +0530

    A9,A10

---
+++ Functions/A10.py
@@ -0,0 +1,7 @@
+# Write a python program to create a function which expects an unknown two number of arguments.
+
+def multiply(a,b,c=1,d=1):
+    e=a*b*c*d
+    print(e)
+
+multiply(2,3)
---
+++ Functions/A9.py
@@ -0,0 +1,9 @@
+# default Argument
+
+def add(a,b,c,d=0):
+    e=a+b+c+d
+    return e
+
+# print(add(66,34))
+print(add(60,30,40))
+print(add(66,25,93,16))
