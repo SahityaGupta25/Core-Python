37cde7fde6b875a00499ee0de285070a06003f81
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-02 09:53:45 +0530

    A10,A11

---
+++ Functions/A11.py
@@ -0,0 +1,11 @@
+# #1. Takes Nothing Returns Nothing
+
+def add():
+    a=int(input("Enter a Number\t"))
+    b=int(input("Enter a Number\t"))
+    c=a+b
+    print(c)
+
+x=add()
+print(x)
+print(type(x))
