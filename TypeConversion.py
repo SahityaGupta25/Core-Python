4ec15f3e84912c2c5582fa7d963f634f0ecedc18
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-21 09:49:45 +0530

    type conversion

---
+++ TypeConversion.py
@@ -0,0 +1,9 @@
+import TypeConversion
+
+math_marks = input('mark ')
+print(type(math_marks))
+
+result =  100 - int(math_marks)
+print(type(result))
+
+print (result)
---
+++ typeconversion1.py
@@ -0,0 +1,5 @@
+# this is a program from lbs to kg
+lbs = input('weight in lbs   ')
+weight = 0.453592 * int(lbs)
+
+print(weight)
