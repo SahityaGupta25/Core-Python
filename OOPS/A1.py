cc60668d48d681eb06c35fb67f11421a4d25b8c8
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-26 10:15:24 +0530

    A1,A2

--- OOPS/A1.py
+++ OOPS/A1.py
@@ -1,17 +1,6 @@
-# Self
+class ABC:
+    def __init__(self) :
+        self.marks=66
 
-class students:
-    def __init__(self,m1) :
-        self.marks1=m1
-        
-    def compare(a,b):
-        if a.marks1>b.marks1:
-            print("a is greater")
-        else:
-            print("b is greater")
-
-a=students(50)
-b=students(80)
-
- 
-
+o1=ABC()
+print(o1.marks)
